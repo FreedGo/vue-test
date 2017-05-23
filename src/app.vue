d2d276755bf47fa6214a5fab8e978d7a05d6777c
Freed <flyxz@126.com>
2017-05-23 15:00:45 +0800

    first commit

---
+++ src/app.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="tpl">
+		{{msg}}
+	</div>
+</template>
+
+<script>
+
+export default{
+	data(){
+		return {
+			msg:'Hello Vuejs'
+		}
+	}
+}
+
+</script>
+
+<style >
+.tpl{
+	color: red;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Freed on 2017/5/23.
+ * E-mail:flyxz@126.com.
+ * GitHub:FreedGo@github.com.
+ */
+//1.0 导入相关包
+import Vue from 'vue';
+import app from './app.vue'
+
+new Vue({
+	el:'#app',
+	render:c=>c(app)
+});
